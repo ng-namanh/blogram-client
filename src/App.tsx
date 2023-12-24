fb1f18618bdcd5e143563f56dc0f1d1611ab7a5c
nna-n5Ember13 <namanh200313@gmail.com>
2023-12-25 02:40:01 +0700

    🎉 init: shadcn setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <div className=' text-red-500'>
+      <Button variant='ghost' size='lg'>
+        Hello
+      </Button>
+    </div>
+  )
+}
+
+export default App
