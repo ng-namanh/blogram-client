47247ab6efa87ff874d201e0dea1885ec2b051e6
nna-n5Ember13 <namanh200313@gmail.com>
2023-12-25 23:58:39 +0700

    ✨ feat: update yarn version

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from './components/ui/button'
 
 function App() {
   return (
-    <div className=' text-red-500'>
+    <div className=' text-red-500 relative h-full font-sans antialiased'>
       <Button size='lg'>Hello</Button>
     </div>
   )
