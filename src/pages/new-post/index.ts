c324a5a906eeb2ccef1c09faf1ab4f99c9eeb413
nna-n5Ember13 <namanh200313@gmail.com>
2024-01-19 00:31:11 +0700

    ✨ feat: rich text editor

---
+++ src/pages/new-post/index.ts
@@ -0,0 +1,1 @@
+export { NewPostPage } from './ui/Page'
--- src/pages/new-post/ui/Page.tsx
+++ src/pages/new-post/ui/Page.tsx
@@ -1,4 +1,42 @@
-function NewPostPage() {
-  return <div>Page</div>
+import Highlight from '@tiptap/extension-highlight'
+import Typography from '@tiptap/extension-typography'
+import { EditorContent, useEditor } from '@tiptap/react'
+import { Document } from '@tiptap/extension-document'
+import StarterKit from '@tiptap/starter-kit'
+
+const CustomDocument = Document.extend({
+  content: 'heading block*'
+})
+
+export function NewPostPage() {
+  const editor = useEditor({
+    extensions: [
+      CustomDocument,
+      StarterKit.configure({
+        document: false
+      }),
+      Highlight,
+      Typography
+    ],
+    content: `
+    <p>
+      Markdown shortcuts make it easy to format the text while typing.
+    </p>
+    `,
+    autofocus: false,
+    editable: true,
+    injectCSS: true,
+    editorProps: {
+      attributes: {
+        class: ' mx-auto focus:outline-none'
+      }
+    }
+  })
+
+  return (
+    <EditorContent
+      editor={editor}
+      className='border border-black w-[800px] p-16 h-[800px]'
+    />
+  )
 }
-export default NewPostPage
