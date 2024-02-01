fd35a9d65330aff330addbc2c5518a950b78f0de
nna-n5Ember13 <namanh200313@gmail.com>
2024-02-02 00:43:30 +0700

    ✨ feat: get posts api

--- src/entities/post/api/postApi.ts
+++ src/entities/post/api/postApi.ts
@@ -1,17 +1,25 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { baseApi } from '@/shared/api/baseApi'
-import { RequestCreatePostBody, ResponseCreatePost } from './types'
+import { baseApi } from "@/shared/api/baseApi";
+import { RequestCreatePostBody, ResponseCreatePost } from "./types";
+import { Post } from "../model/types";
 
 export const postApiSlice = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     createPost: builder.mutation<ResponseCreatePost, RequestCreatePostBody>({
       query: (body) => ({
-        url: 'post/new',
-        method: 'post',
-        body
-      })
-    })
-  })
-})
+        url: "post/new",
+        method: "post",
+        body,
+      }),
+    }),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    getPosts: builder.query<Post[], any>({
+      query: (params) => ({
+        url: "post",
+        method: "get",
+        params,
+      }),
+    }),
+  }),
+});
 
-export const { useCreatePostMutation } = postApiSlice
+export const { useCreatePostMutation, useGetPostsQuery } = postApiSlice;
