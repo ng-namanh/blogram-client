151c7da6f3d1d95194f702664035498239170395
nna-n5Ember13 <namanh200313@gmail.com>
2024-02-01 23:52:52 +0700

    ✨ feat: side bar layout

--- src/shared/ui/layouts/Layout.tsx
+++ src/shared/ui/layouts/Layout.tsx
@@ -1,30 +1,35 @@
-import { cn } from '@/shared/lib/utils'
-import { type ReactNode } from 'react'
-import { Outlet } from 'react-router-dom'
+import { cn } from "@/shared/lib/utils";
+import { type ReactNode } from "react";
+import { Outlet as ArticleList } from "react-router-dom";
 
 type Props = {
-  headerSlot?: ReactNode
-  className?: string
-  itemStart?: boolean
-}
+  headerSlot?: ReactNode;
+  className?: string;
+  itemStart?: boolean;
+  rightSidebar?: ReactNode;
+  leftSidebar?: ReactNode;
+  isHomePage?: boolean;
+};
 
 function Layout(props: Props) {
   return (
     <div
       className={cn(
-        `min-h-screen flex items-start justify-center flex-col ${props.className}`
+        `flex min-h-screen flex-col items-start justify-center ${props.className}`,
       )}
     >
       {props.headerSlot}
       <div
-        className={`max-w-screen-sm lg:max-w-screen-lg mx-auto my-0 flex-1 flex justify-center ${
-          props.itemStart ? 'items-start' : 'items-center'
+        className={`mx-auto my-0 ${props.isHomePage ? "grid-cols-layout grid gap-4" : "flex justify-center"} max-w-screen-sm flex-1 p-4 lg:max-w-[1280px] ${
+          props.itemStart ? "items-start" : "items-center"
         }`}
       >
-        <Outlet />
+        {props.leftSidebar}
+        <ArticleList />
+        {props.rightSidebar}
       </div>
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
