aae9e67b0e6d07d0d8ce82de4bed0d5b24bfe3c1
nna-n5Ember13 <namanh200313@gmail.com>
2024-02-04 23:40:00 +0700

    🐳 chore:prettier quote dble

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,10 +1,10 @@
-import { createApi } from '@reduxjs/toolkit/query/react'
-import { SESSION_TAG } from './tags'
-import { baseQueryWithReauth } from './baseQueryWithReauth'
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { SESSION_TAG } from "./tags";
+import { baseQueryWithReauth } from "./baseQueryWithReauth";
 
 export const baseApi = createApi({
-  tagTypes: [SESSION_TAG],
-  reducerPath: 'api',
+  tagTypes: [SESSION_TAG, "Post"],
+  reducerPath: "api",
   baseQuery: baseQueryWithReauth,
-  endpoints: () => ({})
-})
+  endpoints: () => ({}),
+});
--- src/widgets/header/Navbar.tsx
+++ src/widgets/header/Navbar.tsx
@@ -1,30 +1,30 @@
-import { Button } from '@/shared/ui'
-import { AlignJustify, BellIcon } from 'lucide-react'
-import { Link } from 'react-router-dom'
-import { DropdownMenuHeader } from './Dropdown'
-import { useAppSelector } from '@/shared/model/hook'
-import Logo from '@/shared/ui/logo/Logo'
-import { selectedUser } from '@/entities/auth/model/slice'
-import HeaderSearchInput from './SearchInput'
-import MobileDrawer from './MobileDrawer'
-import { useState } from 'react'
+import { Button } from "@/shared/ui";
+import { AlignJustify, BellIcon } from "lucide-react";
+import { Link } from "react-router-dom";
+import { DropdownMenuHeader } from "./Dropdown";
+import { useAppSelector } from "@/shared/model/hook";
+import Logo from "@/shared/ui/logo/Logo";
+import { selectedUser } from "@/entities/auth/model/slice";
+import HeaderSearchInput from "./SearchInput";
+import MobileDrawer from "./MobileDrawer";
+import { useState } from "react";
 
 function Header() {
-  const user = useAppSelector(selectedUser)
-  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const user = useAppSelector(selectedUser);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   function handleClick(): void {
-    setIsOpen(!isOpen)
+    setIsOpen(!isOpen);
   }
 
   return (
-    <div className='border-b shadow-sm w-full'>
-      <div className='max-w-screen-sm lg:max-w-[1240px] md:max-w-[960px] mx-auto my-auto py-2 flex px-2'>
-        <div className='flex gap-2 items-center justify-center'>
+    <div className="w-full border-b shadow-sm">
+      <div className="mx-auto my-auto flex max-w-screen-sm px-2 py-2 md:max-w-[960px] lg:max-w-[1240px]">
+        <div className="flex items-center justify-center gap-2">
           <AlignJustify
             width={40}
             onClick={handleClick}
-            className='block md:hidden'
+            className="block md:hidden"
           />
 
           <Logo width={50} />
@@ -32,29 +32,29 @@ function Header() {
         </div>
         {isOpen && <MobileDrawer setIsOpen={setIsOpen} />}
         {user ? (
-          <div className='flex flex-1 items-end justify-end gap-3'>
-            <Link to='/new'>
-              <Button variant='outline' className='hidden md:block'>
+          <div className="flex flex-1 items-end justify-end gap-3">
+            <Link to="/new">
+              <Button variant="outline" className="hidden md:block">
                 Create Post
               </Button>
             </Link>
-            <Button variant='ghost' className='p-2'>
+            <Button variant="ghost" className="p-2">
               <BellIcon />
             </Button>
             <DropdownMenuHeader />
           </div>
         ) : (
-          <div className='flex flex-1 items-end justify-end'>
-            <Link to='/auth/login'>
-              <Button variant='ghost'>Log in</Button>
+          <div className="flex flex-1 items-end justify-end">
+            <Link to="/auth/login">
+              <Button variant="ghost">Log in</Button>
             </Link>
-            <Link to='/auth/register'>
-              <Button variant='outline'>Create account</Button>
+            <Link to="/auth/register">
+              <Button variant="outline">Create account</Button>
             </Link>
           </div>
         )}
       </div>
     </div>
-  )
+  );
 }
-export default Header
+export default Header;
