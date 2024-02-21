d64d1d6feea4df2dde18dab3c7bf8e16962684b8
nna-n5Ember13 <namanh200313@gmail.com>
2024-02-21 12:54:34 +0700

    🌈 style: layout style and tailwind prettier update

--- src/shared/ui/input.tsx
+++ src/shared/ui/input.tsx
@@ -1,22 +1,22 @@
-import * as React from 'react'
+import * as React from 'react';
 
-import { cn } from '@/shared/lib/utils'
+import { cn } from '@/shared/lib/utils';
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
-  Slot?: React.ReactNode
-  slotWrapperClassName?: string
+  Slot?: React.ReactNode;
+  slotWrapperClassName?: string;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, Slot, slotWrapperClassName, ...props }, ref) => {
     return (
-      <div className='relative'>
+      <div className="relative">
         <input
           type={type}
           className={cn(
-            'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#3b49df] disabled:cursor-not-allowed disabled:opacity-50',
-            className
+            'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary disabled:cursor-not-allowed disabled:opacity-50',
+            className,
           )}
           ref={ref}
           {...props}
@@ -24,17 +24,17 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         {Slot && (
           <div
             className={cn(
-              'absolute inset-y-0 right-0 flex items-center pointer-events-auto h-9',
-              slotWrapperClassName
+              'pointer-events-auto absolute inset-y-0 right-0 flex h-9 items-center',
+              slotWrapperClassName,
             )}
           >
             {Slot}
           </div>
         )}
       </div>
-    )
-  }
-)
-Input.displayName = 'Input'
+    );
+  },
+);
+Input.displayName = 'Input';
 
-export { Input }
+export { Input };
