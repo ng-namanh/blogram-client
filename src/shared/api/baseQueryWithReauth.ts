9c2f32e19dd0b30d47b2e60211c5d1bfc02f781a
nna-n5Ember13 <namanh200313@gmail.com>
2024-02-24 10:18:50 +0700

    🦄 refactor: sending token in body

--- src/shared/api/baseQueryWithReauth.ts
+++ src/shared/api/baseQueryWithReauth.ts
@@ -2,50 +2,50 @@ import {
   BaseQueryApi,
   FetchBaseQueryMeta,
   type FetchArgs,
-  type FetchBaseQueryError
-} from '@reduxjs/toolkit/query'
-import { baseQuery } from './baseQuery'
-import { QueryReturnValue } from 'node_modules/@reduxjs/toolkit/dist/query/baseQueryTypes'
-import { RootState } from '@/app/appStore'
-import { logout, setCredentials } from '@/entities/auth/model/slice'
+  type FetchBaseQueryError,
+} from '@reduxjs/toolkit/query';
+import { baseQuery } from './baseQuery';
+import { QueryReturnValue } from 'node_modules/@reduxjs/toolkit/dist/query/baseQueryTypes';
+import { RootState } from '@/app/appStore';
+import { logout, setCredentials } from '@/entities/auth/model/slice';
 
-const AUTH_ERROR_CODES = new Set([401])
+const AUTH_ERROR_CODES = new Set([401]);
 
 export async function baseQueryWithReauth(
   args: string | FetchArgs,
   api: BaseQueryApi,
-  extraOptions: object
+  extraOptions: object,
 ): Promise<QueryReturnValue<unknown, FetchBaseQueryError, FetchBaseQueryMeta>> {
-  let result = await baseQuery(args, api, extraOptions)
+  let result = await baseQuery(args, api, extraOptions);
 
   if (
     typeof result.error?.status === 'number' &&
     AUTH_ERROR_CODES.has(result?.error?.status)
   ) {
-    // send refresh token request
-    console.log('sending refresh token')
+    console.log('sending refresh token');
 
-    const user = (api.getState() as RootState).auth.user
+    const user = (api.getState() as RootState).auth.user;
+    const token = (api.getState() as RootState).auth.token;
 
     if (user) {
       const refreshArgs: FetchArgs = {
         url: '/auth/refresh',
         method: 'POST',
-        body: user
-      }
+        body: token,
+      };
 
-      const refreshResult = await baseQuery(refreshArgs, api, extraOptions)
-      console.log(refreshResult)
+      const refreshResult = await baseQuery(refreshArgs, api, extraOptions);
+      console.log(refreshResult);
 
       if (refreshResult?.data) {
-        api.dispatch(setCredentials({ ...refreshResult.data, user }))
-        result = await baseQuery(args, api, extraOptions)
+        api.dispatch(setCredentials({ ...refreshResult.data, user }));
+        result = await baseQuery(args, api, extraOptions);
       }
     } else {
-      console.log('no user found, logging out')
-      api.dispatch(logout())
+      console.log('no user found, logging out');
+      api.dispatch(logout());
     }
   }
 
-  return result
+  return result;
 }
