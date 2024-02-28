3377b86d53eaae57983874dabd65e7b0058f7a7d
nna-n5Ember13 <namanh200313@gmail.com>
2024-02-29 00:46:09 +0700

    ✨ feat: cover image for post page

--- src/pages/post/Page.tsx
+++ src/pages/post/Page.tsx
@@ -13,6 +13,12 @@ export function PostPage() {
     <div className="grid grid-cols-postLayout gap-4">
       <aside className=""></aside>
       <main className=" min-h-screen w-[806px] rounded-md bg-secondary">
+        <div
+          className="h-[300px] rounded-t-md border-t bg-cover bg-center"
+          style={{
+            backgroundImage: `url(${data?.post.coverImageUrl})`,
+          }}
+        ></div>
         <div className=" px-16 pt-8">
           <div>
             <p className="cursor-pointer text-lg hover:text-primary">
